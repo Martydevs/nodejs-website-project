1d2f0fbc20713e15b737cc2e2daa1eccbd5582ab
tmdev <tmdev64@gmail.com>
2022-11-02 12:40:02 -0600

    remove port constant for static port number

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
-import { PORT } from "./config.js";
+
 import router from "./routes/routes.js";
 
 const app = express();
@@ -15,6 +15,6 @@ app.use(router);
 
 app.use(express.static(join(__dirname, "public")));
 
-app.listen(PORT, () => {
-  console.log(`Server on port ${PORT}`);
+app.listen(3000, () => {
+  console.log(`Server on port ${3000}`);
 });
